40a26cf22e6d609035056527fe0bec2d98eb47e1
Pietari Immonen <pietari.immonen@gmail.com>
2025-10-16 12:48:53 +0300

    fix: CI?

--- packages/core/src/event.ts
+++ packages/core/src/event.ts
@@ -1,10 +1,11 @@
 import { event } from "sst/event";
 import { ZodValidator } from "sst/event/validator";
+import type { ZodType } from "zod";
 
 import { ActorContext } from "./actor";
 
 export const defineEvent = event.builder({
-  validator: ZodValidator,
+  validator: ZodValidator as (schema: ZodType) => typeof ZodValidator,
   metadata() {
     return {
       actor: ActorContext.use(),
