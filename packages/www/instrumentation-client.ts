f9477df98e7cafccd49f66a7c0cbf1e08755601a
Pietari Immonen <pietari.immonen@gmail.com>
2025-10-29 21:31:33 +0200

    fix: posthog

--- packages/www/instrumentation-client.ts
+++ packages/www/instrumentation-client.ts
@@ -1,9 +1,6 @@
 import posthog from "posthog-js";
 
 posthog.init("phc_HfnQrxPQJo2I01PXljw5HcaiN3SVGcolrgh132kmdsB", {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
+  api_host: "https://eu.i.posthog.com",
   defaults: "2025-05-24",
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
 });
